137b18153d843d94ec80c8784ed482745210379b
herdiardn <121550473+herdiardn@users.noreply.github.com>
2023-04-09 22:01:51 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul8_1302210078
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
